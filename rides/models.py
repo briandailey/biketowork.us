7d960a9c80488f468e71af7810b2a6b63ff17ed1
Brian Dailey <brian@stratasan.com>
2014-07-22 21:28:07 -0500

    Adding a str method to ride.

--- rides/models.py
+++ rides/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 class Ride(models.Model):
     distance = models.DecimalField(max_digits=5, decimal_places=2)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    def __str__(self):
+        return "{minutes}m, {distance} miles".format(
+                minutes=round((self.start_time - self.end_time).seconds/60, 2),
+                distance=self.distance)
 
 
