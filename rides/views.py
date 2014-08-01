fa4e31fd973e481149d27b0afc05e02c723f42fd
Brian Dailey <brian@stratasan.com>
2014-07-31 20:45:22 -0500

    pass rides variable to the template.

--- rides/views.py
+++ rides/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, render_to_response, HttpResponse
+
+# because the models are in the same module, we can use a relative import here.
 from .models import Ride
 
 def recent(request):
+    rides = Ride.objects.order_by('-start_time')[:5]
+    return render_to_response('rides/recent.html', {
+        'rides': rides,
+    })
+
     return HttpResponse('leeeroy jenkins!')
