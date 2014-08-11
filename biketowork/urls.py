444baf6b35679119c7303b6cc29bc10090c1bb7f
Brian Dailey <brian@stratasan.com>
2014-08-10 22:49:55 -0500

    Add view, template, and URL route.

--- biketowork/urls.py
+++ biketowork/urls.py
@@ -3,15 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from rides.views import recent
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'biketowork.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', recent),
+    url(r'^$', 'rides.views.recent', name='recent_rides'),
+    url(r'^new/$', 'rides.views.new', name='new_ride'),
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
--- rides/views.py
+++ rides/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render, render_to_response, HttpResponse
 
 # because the models are in the same module, we can use a relative import here.
 from .models import Ride
+from .forms import RideForm
 
 def recent(request):
     rides = Ride.objects.order_by('-start_time')[:5]
     return render(request, 'rides/recent.html', {
             'rides': rides,
+    })
+
+def new(request):
+    form = RideForm()
+    return render(request, 'rides/new.html', {
+            'form': form,
     })
