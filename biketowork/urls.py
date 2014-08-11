59b1b19c9005a4a0afe1fdc15f946aaba00aad53
Brian Dailey <brian@stratasan.com>
2014-08-10 20:39:24 -0500

    add convenient login/logout urls and links.

--- biketowork/urls.py
+++ biketowork/urls.py
@@ -13,8 +13,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', recent),
 
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 
     url('', include('social.apps.django_app.urls', namespace='social')),
 )
