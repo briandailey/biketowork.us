615dbf2d95b87b6dcde2d0054137d2316554bb88
Brian Dailey <brian@stratasan.com>
2014-08-10 21:20:47 -0500

    Adding setUp for Ride test.

--- rides/tests.py
+++ rides/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from rides.models import Ride
+
+
+class TestRide(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username='test',
+            password='test',
+            email='test@dailytechnology.net',
+        )
+        Ride.objects.create(
+            user=user,
+            distance=5.5,
+            start_time=datetime.datetime.now(),
+            end_time=datetime.datetime.now() + timedelta(hours=1.5)
+        )
